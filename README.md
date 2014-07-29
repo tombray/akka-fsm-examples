1ef348664fb658c7eef0498e68f8ca04d3de27fd
Tom Bray <tom@tombray.com>
2014-07-28 20:44:42 -0700

    house keeping

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 #Akka FSM Examples
 I really fell in love with Akka when I discovered its Finite State Machine DSL. I created this project to provide examples of how to 
 use the FSM trait with your actors and how to test your state machines. One of the classic examples of a state machine is an elevator, 
-so I thought I'd start there. I developed my Elevator test first which allowed me to discover a nice design and to be confident as I refactored.
+so I thought I'd start there. I developed my Elevator test-first which allowed me to discover a nice design and to be confident as I refactored.
 What I ended up with is a fully functioning elevator that responds to requests to get on and off in a pretty efficient manner -- certainly more efficient
-than the real elevators in the building where I work!
+than the real elevators in the building where I work! And it's less than 100 lines of code!
 
 Please start by taking a look at the elevator itself: [ElevatorActor.scala](https://github.com/tombray/akka-fsm-examples/blob/master/src/main/scala/com/tombray/examples/akka/ElevatorActor.scala)
 
--- src/main/scala/com/tombray/examples/akka/ElevatorActor.scala
+++ src/main/scala/com/tombray/examples/akka/ElevatorActor.scala
@@ -22,6 +22,21 @@ class ElevatorActor extends Actor with ActorLogging with FSM[State,Data]{
   when(GoingUp) (going(Up))
   when(GoingDown) (going(Down))
 
+  //whether we're GoingUp or GoingDown, the logic to handle incoming events is very similar; this function allows us to stay DRY
+  def going(sameDirection:Direction):StateFunction = {
+    val oppositeDirection = if (sameDirection == Up) Down else Up
+    def moreRequestsInSameDirectionFn(floor:Int, direction:Direction, requests:Set[Request]) = if (direction == Up) hasRequestsForHigherFloors(floor, requests) else hasRequestsForLowerFloors(floor, requests)
+    def needToSwitchDirections( direction:Direction, floor:Int, requests:Set[Request]) = !moreRequestsInSameDirectionFn(floor,direction,requests) && requests.contains(GetOn(floor,oppositeDirection))
+    def removeAllRequestsForFloor(floor:Int, requests:Set[Request]) = requests.filter(_.floor != floor)
+    def hasRequestForFloor(floor:Int, requests:Set[Request], direction:Direction): Boolean = requests.contains(GetOff(floor)) || requests.contains(GetOn(floor, direction))
+
+    return {
+      case Event(request:Request, d) => stay using Data(sameDirection, d.currentFloor, d.requests + request)
+      case Event(ArrivedAtFloor(floor), d) if hasRequestForFloor(floor,d.requests,sameDirection) => goto(Open) using Data(sameDirection, floor, removeAllRequestsForFloor(floor, d.requests))
+      case Event(ArrivedAtFloor(floor), d) if needToSwitchDirections(d.direction, floor, d.requests) => goto(Open) using Data(oppositeDirection, floor, removeAllRequestsForFloor(floor, d.requests))
+    }
+  }
+
   when(Open, 3 seconds) {
     case Event(request:Request, Data(NoDirection,currentFloor,requests)) if request.floor < currentFloor => stay using Data(Down, currentFloor, requests + request)
     case Event(request:Request, Data(NoDirection,currentFloor,requests)) if request.floor > currentFloor => stay using Data(Up, currentFloor, requests + request)
@@ -35,21 +50,6 @@ class ElevatorActor extends Actor with ActorLogging with FSM[State,Data]{
         else if (hasRequestsForLowerFloors(d.currentFloor, d.requests)) goto(GoingDown) using d.copy(direction = Down)
         else stay() //TODO should goto(Idle), write a test first
       }
-    }
-  }
-
-  //whether we're GoingUp or GoingDown, the logic to handle incoming events is very similar; this function allows us to stay DRY
-  def going(sameDirection:Direction):StateFunction = {
-    val oppositeDirection = if (sameDirection == Up) Down else Up
-    def moreRequestsInSameDirectionFn(floor:Int, direction:Direction, requests:Set[Request]) = if (direction == Up) hasRequestsForHigherFloors(floor, requests) else hasRequestsForLowerFloors(floor, requests)
-    def needToSwitchDirections( direction:Direction, floor:Int, requests:Set[Request]) = !moreRequestsInSameDirectionFn(floor,direction,requests) && requests.contains(GetOn(floor,oppositeDirection))
-    def removeAllRequestsForFloor(floor:Int, requests:Set[Request]) = requests.filter(_.floor != floor)
-    def hasRequestForFloor(floor:Int, requests:Set[Request], direction:Direction): Boolean = requests.contains(GetOff(floor)) || requests.contains(GetOn(floor, direction))
-
-    return {
-      case Event(request:Request, d) => stay using Data(sameDirection, d.currentFloor, d.requests + request)
-      case Event(ArrivedAtFloor(floor), d) if hasRequestForFloor(floor,d.requests,sameDirection) => goto(Open) using Data(sameDirection, floor, removeAllRequestsForFloor(floor, d.requests))
-      case Event(ArrivedAtFloor(floor), d) if needToSwitchDirections(d.direction, floor, d.requests) => goto(Open) using Data(oppositeDirection, floor, removeAllRequestsForFloor(floor, d.requests))
     }
   }
 
